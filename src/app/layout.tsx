4ce5b64e878c0373247bbe421a718b17258290d8
Jakub Kozel <louben17@gmail.com>
2025-07-24 05:27:22 +0200

    fix: clean contact page design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,11 +50,11 @@ export default function RootLayout({
          {children}
        </main>
        
-        <footer className="fixed bottom-4 left-4 z-20">
-          <p className="text-lg text-red-500 bg-white p-2 border">
-            © 2025 / jakubkozel@seznam.cz / 728890062
-          </p>
-        </footer>
+<footer className="absolute bottom-4 left-4 z-20">
+  <p className="text-lg text-red-500 bg-white p-2 border">
+    © 2025 / jakubkozel@seznam.cz / 728890062
+  </p>
+</footer>
      </body>
    </html> 
  );
